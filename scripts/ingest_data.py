faa149bd9c0373fbda728eca73d38c62a03e0ab2
Dobromir Velkov <itszerop81@gmail.com>
2024-06-17 23:54:11 +0300

    Update ingest_data.py

--- scripts/ingest_data.py
+++ scripts/ingest_data.py
@@ -1,26 +1,20 @@
 from pyspark.sql import SparkSession
-import requests
-import pandas as pd
 
-# Initializing session
+# 
 spark = SparkSession.builder \
     .appName("FinancialDataIngestion") \
     .getOrCreate()
 
-# Fetching data from API
-def fetch_data(api_key, symbol, function="TIME_SERIES_DAILY"):
-    url = f"https://www.alphavantage.co/query?function={function}&symbol={symbol}&apikey={api_key}&datatype=csv"
-    response = requests.get(url)
-    with open(f"data/{symbol}_daily.csv", 'wb') as f:
-        f.write(response.content)
-
-def read_data(symbol):
-    df = spark.read.csv(f"data/{symbol}_daily.csv", header=True, inferSchema=True)
+def fetch_data(url, output_path):
+    # Using spark for it to read data from a url
+    df = spark.read.csv(url, header=True, inferSchema=True)
+    # Saving raw data to a prequel file for higher performance
+    df.write.mode("overwrite").parquet(output_path)
     return df
 
 if __name__ == "__main__":
-    api_key = "your_api_key"
-    symbol = "AAPL"
-    fetch_data(api_key, symbol)
-    df = read_data(symbol)
+    #historical stock data for Apple from Yahoo Finance
+    url = "https://query1.finance.yahoo.com/v7/finance/download/AAPL?period1=1577836800&period2=1609459200&interval=1d&events=history&includeAdjustedClose=true"
+    output_path = "data/large_financial_data.parquet"
+    df = fetch_data(url, output_path)
     df.show()
